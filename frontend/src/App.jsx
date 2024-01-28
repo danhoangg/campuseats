f77f747c9bbc69b88b7fe068821ffc7856718825
danhoangg <hoang.daniel2005@gmail.com>
2024-01-28 05:50:18 +0000

    profile page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { withAuthenticationRequired } from '@auth0/auth0-react';
 
 import Navbar from "./NavBar";
 import ChatContainer from "./ChatContainer";
 import Login from './Login';
+import Profile from './Profile';
 
 function App() {
+  const ProtectedChatContainer = withAuthenticationRequired(ChatContainer, {});
+  const ProtectedProfile = withAuthenticationRequired(Profile, {});
+
   return (
     <Router>
       <div className='App bg-red-200 h-screen'>
         <Navbar />
         <Routes>
-          <Route exact path="/matches" element={<ChatContainer />} />
+          <Route exact path="/matches" element={<ProtectedChatContainer />} />
           <Route exact path="/matching" element={<p>hi</p>} />
           <Route exact path="/login" element={<Login />} />
+          <Route exact path="/profile" element={<ProtectedProfile />} />
         </Routes>
       </div>
     </Router>
